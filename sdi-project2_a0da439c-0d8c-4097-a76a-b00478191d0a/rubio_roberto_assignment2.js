a8efe93eb574c5a93bc86520ed3b42720a541a16
Roberto Rubio <toolhwp9630@fullsail.edu>
2014-12-06 12:42:44 -0600

    Return Confirmation
    confirming data is being returned from function.

--- sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/rubio_roberto_assignment2.js
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/rubio_roberto_assignment2.js
@@ -5,15 +5,14 @@
 
 
 //My Variables
-
 var social = "Google+";
 var theBest = true;
 var numFriends = 5;
+var returnBoolean;
 
-
-//Procedure
+//Procedure 
 var myProcedure = function () {
-    myPrompt = prompt("Which Social Media Website is the best for posting pics?", social);
+    myPrompt = prompt("Which Social Media website is the best for posting pics?", social);
     if (myPrompt === social) {
         console.log("I know I love it too!");
     } else {
@@ -21,8 +20,24 @@ var myProcedure = function () {
     }
 };
 
+//Confirm and Boolean Function While Loop
+var ask = function () {
+    var myConfirm = confirm("Let's try this again. Isn't " + social + " the best?");
+    return myConfirm;
+};
+var returnBoolean = function (message, myConfirm) {
+    console.log(message + " is " + myConfirm);
+};
+
 
 //Main Code
+console.log("I have a bunch of pictures I want to post online and I'd like you to help me out.");
 
 //Procedure
 myProcedure();
+
+//Confirm
+var myConfirm = ask();
+
+//Boolean Function Return
+returnBoolean("This", myConfirm);
