8c00e8aabf94ca67e7e9c2e697ff57df6e62af79
Roberto Rubio <toolhwp9630@fullsail.edu>
2014-12-05 21:35:44 -0600

    Starting Point
    This is my starting point for Project 2

---
+++ sdi-project2_a0da439c-0d8c-4097-a76a-b00478191d0a/rubio_roberto_assignment2.js
@@ -0,0 +1,59 @@
+//  Roberto Rubio
+//  SDI 1412
+//  12/05/14
+//  Project 2
+
+
+//My Variables
+
+ var social = "\"Google+\"var";
+ 
+ 
+ 
+ //My Procedure
+ 
+ 
+
+
+
+
+/*//alert("JavaScript works!");
+
+//global variables
+var returnValueVar;
+
+
+//my function
+// returning values on flow chart
+var division = function(firstNumber, secondNumber) {
+    
+    if (secondNumber === 0) {
+        
+        return "can't divide by zero!"
+        
+    } else {
+        
+        var outcome = firstNumber / secondNumber;
+        return outcome;
+
+    }
+    
+};
+
+var myProcedure = function () { // procedures don't have a return value
+    
+    console.log("hello World");
+    
+};
+
+//my main code that calls the function
+myProcedure(); // procedure doesn't have any value saved and carried over only direct output.
+
+returnValueVar = division(150, 15); // returning values on flow chart
+
+console.log("The answer is " + returnValueVar + ".");
+
+returnValueVar = division(250, returnValueVar);
+
+console.log("The second answer is " + returnValueVar + ".");
+*/
